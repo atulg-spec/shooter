c479a3f0950318d0d7e3fa47957b9c88f3951d99
atul <atulg0736@gmail.com>
2024-11-21 17:31:03 +0530

    user admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,20 +1,44 @@
 from django.contrib import admin
 from .models import EmailAccounts, AudienceData, Tags, tags_data, Messages, Campaign
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserChangeForm
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User, Permission
+from django import forms
+from django.utils.translation import gettext_lazy as _
 
-# Customizing the UserAdmin
-class CustomUserAdmin(UserAdmin):
-    form = CustomUserChangeForm
-    # Optionally, you can modify the fieldsets to include the permissions field
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('permissions',)}),
+# Custom form for the User model
+class CustomUserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    # Customize the permissions field
+    permissions = forms.ModelMultipleChoiceField(
+        queryset=Permission.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        label=_("User Permissions"),
     )
 
-# Register the custom UserAdmin
+# Custom UserAdmin class
+class CustomUserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+
+    # Optionally customize the fieldsets to include the permissions field
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+# Unregister the default User admin
 admin.site.unregister(User)
+
+# Register the custom User admin
 admin.site.register(User, CustomUserAdmin)
+
 
 @admin.register(EmailAccounts)
 class EmailAccountsAdmin(admin.ModelAdmin):
